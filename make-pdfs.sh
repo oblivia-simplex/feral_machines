13daf9e0903c2c9b3b729050d5e0b6f232b2f3ce
Lucca Fraser <lucca.fraser@gmail.com>
2022-01-19 13:10:04 -0400

    Cronjob sync of feral machines from notes vault: Wed 19 Jan 2022 01:10:04 PM AST

--- make-pdfs.sh
+++ make-pdfs.sh
@@ -4,6 +4,13 @@ cd posts
 for p in *.md ; do
 f=$(basename "$p").pdf
 echo "[+] Converting $p --> $f"
-pandoc --pdf-engine=xelatex "$p" -o "../static/pdfs/$f"
+tmp=$(mktemp).md
+cat $p | sed "s/author: me/author: Olivia Lucca Fraser/" > $tmp
+pandoc --pdf-engine=xelatex "$tmp" -o "../static/pdfs/$f"
+rm $tmp
 done
+
+cd ../static/pdfs
+
+rename "s/md\.pdf$/.pdf/" *.md.pdf
 
